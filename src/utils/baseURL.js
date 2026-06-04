2093331078b5581afca604da18215a18f71d481d
98chawbelar <130055118+98chawbelar@users.noreply.github.com>
2026-06-04 20:58:58 +0700

    update apis

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   if (import.meta.env.DEV) {
     return "http://localhost:5000/api";
   } else {
-    return "https://mrbs-server.vercel.app";
+    return "https://mrbs-server.vercel.app/api";
   }
 };
 
