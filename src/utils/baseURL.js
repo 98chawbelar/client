e9e1604fb0fe9d012abefb1227c3b9f41d14a4d5
98chawbelar <130055118+98chawbelar@users.noreply.github.com>
2026-06-04 21:03:51 +0700

    update base url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,9 +1,9 @@
 const getBaseUrl = () => {
   if (import.meta.env.DEV) {
     return "http://localhost:5000/api";
-  } else {
-    return "https://mrbs-server.vercel.app/api";
   }
+
+  return "https://mrbs-server.vercel.app/api";
 };
 
 export default getBaseUrl;
